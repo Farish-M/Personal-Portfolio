67f3930b26f88807dfc475f869d023df03e89098
Farish-M <farishmohamedrozlan@gmail.com>
2026-06-08 05:19:59 +0800

    bugfix: fixed path traversal, fixing bug that came from moving index.html to root

--- src/components/pages/projects-js/projectData.js
+++ src/components/pages/projects-js/projectData.js
@@ -2,7 +2,7 @@ const projects = [
     {
         title: "RNA 3DB",
         description: "A 3D visualisation web-app created in React and Django, using data from RNA3DB repository. This application uses Molstar and Sequence Annotation Viewer to visualise RCSB's RNA data, allowing for researchers to visualise RNA chains",
-        image: "../public/assets/project-screenshots/RNA-3DB-Preview.png",
+        image: "/public/assets/project-screenshots/RNA-3DB-Preview.png",
         tags: ["React", "Molstar", "SAV", "Django"],
         githubLink: "https://github.com/marcellszi/RNA3DB-visualisations-project"
     },
